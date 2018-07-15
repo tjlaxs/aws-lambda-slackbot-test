417e85545b8bceb7cd04949e0ad8408fd1548abf
Tero Laxström <tlax@neonpeons.com>
2018-07-15 21:45:59 +0300

    Some refactoring
    - created Slack module
    - created APIGateway module
    
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- src/Main.purs
+++ src/Main.purs
@@ -1,48 +1,15 @@
 module Main where
 
 import Prelude
-import Data.Array (head, (!!))
-import Foreign (F, Foreign, ForeignError, readBoolean, readString)
-import Foreign.Index ((!))
-import Lambda (Context, Response(..), fail, succeed)
+import Foreign (Foreign, ForeignError)
 import Effect (Effect)
 import Data.Either (Either(..))
 import Control.Monad.Except (runExcept)
 import Data.List.Types (NonEmptyList)
-import Data.Tuple (Tuple(..))
-import Data.Maybe (Maybe, fromMaybe)
-import Data.String.Common (split)
-import Data.String.Pattern (Pattern(..))
-import Global.Unsafe (unsafeDecodeURI)
 
-newtype Event = Event
-  { resource :: String
-  , path :: String
-  , httpMethod :: String
-  , body :: String
-  , isBase64Encoded :: Boolean
-  }
-
-readEvent :: Foreign -> F Event
-readEvent value = do
-  resource <- value ! "resource" >>= readString
-  path <- value ! "path" >>= readString
-  httpMethod <- value ! "httpMethod" >>= readString
-  body <- value ! "body" >>= readString
-  isBase64Encoded <- value ! "isBase64Encoded" >>= readBoolean
-  pure $ Event { resource, path, httpMethod, body, isBase64Encoded }
-
-tuplify :: Array String -> Tuple String (Maybe String)
-tuplify xs = Tuple (fromMaybe "" $ head xs) (xs !! 1)
-
-splitKeyvals :: String -> (Array String)
-splitKeyvals = split (Pattern "=")
-
-splitParams :: String -> Array String
-splitParams = split (Pattern "&")
-
-decodeSlackCommand :: String -> Array (Tuple String (Maybe String))
-decodeSlackCommand = map tuplify <<< map (map unsafeDecodeURI) <<< map splitKeyvals <<< splitParams
+import Lambda (Context, Response(..), fail, succeed)
+import APIGateway (Event(..), readEvent)
+import Slack (decodeSlackCommand)
 
 handler :: Context -> Foreign -> Effect Unit
 handler ctx evt = do
---
+++ src/Slack.purs
@@ -0,0 +1,23 @@
+module Slack where
+
+import Prelude (($), (<<<), map)
+import Data.Array (head, (!!))
+import Data.Maybe (Maybe, fromMaybe)
+import Data.String.Pattern (Pattern(..))
+import Data.String.Common (split)
+import Data.Tuple (Tuple(..))
+import Global.Unsafe (unsafeDecodeURI)
+
+decodeSlackCommand :: String -> Array (Tuple String (Maybe String))
+decodeSlackCommand =
+  map tuplify <<< map (map unsafeDecodeURI) <<< map splitKeyvals <<< splitParams
+
+  where
+    tuplify :: Array String -> Tuple String (Maybe String)
+    tuplify xs = Tuple (fromMaybe "" $ head xs) (xs !! 1)
+
+    splitKeyvals :: String -> (Array String)
+    splitKeyvals = split (Pattern "=")
+
+    splitParams :: String -> Array String
+    splitParams = split (Pattern "&")
