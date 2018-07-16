28746d4d15871b6c75a6a3dab75eddf167a84a02
Tero Laxström <tlax@neonpeons.com>
2018-07-16 20:57:04 +0300

    Added types for Slack commands
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- src/Slack.purs
+++ src/Slack.purs
@@ -1,12 +1,42 @@
 module Slack where
 
-import Prelude (($), (<<<), map)
+import Prelude (class Show, ($), (<<<), map)
 import Data.Array (head, (!!))
 import Data.Maybe (Maybe, fromMaybe)
 import Data.String.Pattern (Pattern(..))
 import Data.String.Common (split)
 import Data.Tuple (Tuple(..))
 import Global.Unsafe (unsafeDecodeURIComponent)
+
+newtype Token = Token String
+instance showToken :: Show Token where
+  show _ = "Token (censored)"
+
+newtype Team = Team
+  { id :: String
+  , domain :: String
+  }
+
+newtype Channel = Channel
+  { id :: String
+  , name :: String
+  }
+
+newtype User = User
+  { id :: String
+  , name :: String
+  }
+
+newtype CommandRequest = CommandRequest
+  { token :: Token
+  , team :: Team
+  , channel :: Channel
+  , user :: User
+  , command :: String
+  , text :: String
+  , responseUrl :: String
+  , triggerId :: String
+  }
 
 decodeSlackCommand :: String -> Array (Tuple String (Maybe String))
 decodeSlackCommand =
